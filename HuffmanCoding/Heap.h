4056430dcc8deababd7fa0b2af3f896534a6bc12
Hamza Beder <134306865+HamzaBeder1@users.noreply.github.com>
2023-08-27 01:50:02 -0700

    Update Heap.h

--- HuffmanCoding/Heap.h
+++ HuffmanCoding/Heap.h
@@ -2,40 +2,32 @@
 // Created by Hamza Beder on 8/8/2023.
 //
 
-#include "Heap.h"
+#ifndef HEAP_H
+#define HEAP_H
+#include <iostream>
+#include <fstream>
 #include <vector>
-using namespace std;
-HeapNode:: HeapNode(){
-        this->c = 0;
-        this->freq = 0;
-        this->left = this->right = nullptr;
-}
+#include <map>
+#include <queue>
 
-HeapNode:: HeapNode(char c, int freq){
-        this->c = c;
-        this->freq = freq;
-        this->left = this->right = nullptr;
-}
+class HeapNode {
+public:
+    char c;
+    int freq;
+    HeapNode * left;
+    HeapNode * right;
+    HeapNode();
+    HeapNode(char c, int freq);
+};
 
-Heap:: Heap(){
+class Heap{
+public:
+    int length;
+    int heap_size;
+    HeapNode ** nodes;
+    Heap(int length);
+    Heap();
+    void minHeapify(int idx);
+};
 
-}
-Heap:: Heap(int length){
-    this->length = length;
-    this->heap_size = 0;
-    this->nodes = new HeapNode*[length];
-}
-
-void Heap:: minHeapify(int idx){
-    int smallest = idx;
-    if(2*idx+1 < this->heap_size && this->nodes[2*idx+1]->freq < this->nodes[smallest]->freq)
-        smallest = 2*idx+1;
-    if(2*idx+2 < this->heap_size && this->nodes[2*idx+2]->freq < this->nodes[smallest]->freq)
-        smallest = 2*idx+2;
-    if(smallest != idx){
-        HeapNode * temp = this->nodes[smallest];
-        this->nodes[smallest] = this->nodes[idx];
-        this->nodes[idx] = temp;
-        minHeapify(smallest);
-    }
-}
+#endif
