6b2c9e47bc87034c6de8af690d1be02caf77f311
Hamza Beder <134306865+HamzaBeder1@users.noreply.github.com>
2023-08-31 15:36:50 -0700

    added comments

--- HuffmanCoding/HuffmanTree.h
+++ HuffmanCoding/HuffmanTree.h
@@ -1,6 +1,8 @@
-//
-// Created by Hamza Beder on 8/8/2023.
-//
+/**
+ * @file HuffmanTree.h
+ * 
+ * @author Hamza Beder
+*/
 
 #ifndef HUFFMANCODING_HUFFMANTREE_H
 #define HUFFMANCODING_HUFFMANTREE_H
@@ -11,19 +13,41 @@
 #include "Heap.h"
 #include "PriorityQueue.h"
 
-
 class HuffmanTree{
 public:
-    HeapNode * root;
-    std::map<std::string, char> decompressingMap;
-    std::map<char, std::string> HuffmanCodes;
-    void countFrequency(std::vector<char> &chars, std::vector<int> &freqs, std::string path);
+    /**
+    * This class will build a HuffmanTree using a priority queue, and the constructed tree will be used
+    * to compress and decompress a file.
+    */
+
+    HeapNode * root; //This is the root of the Huffman Tree.
+    std::map<char, std::string> HuffmanCodes;   //This maps characters in the file to Huffman codes, used for compressing.
+    std::map<std::string, char> decompressingMap; //This maps Huffman codes to characters in the file, used for decompressing.
+
+    /**
+     * This method will store the characters and their associated frequencies in the vectors chars and freqs
+    */
+    void countFrequency(std::vector<char> &chars, std::vector<int> &freqs, std::string path); 
+    
+    /**
+     * This method constructs the Huffman Tree
+    */
     void createTree(std::vector <char> chars, std::vector<int> freqs, std:: string path);
+
+    /**
+     * This method will traverse the Huffman Tree to map codes<->characters
+    */
     void getCodes(HeapNode * currNode, std::string code);
+
+    /**
+     * This method is used to compress a file.
+    */
     void compress(std::string path);
+
+    /**
+     * This method is used to decompress a file
+    */
     void decompress();
 };
 
-
-#endif //HUFFMANCODING_HUFFMANTREE_H
-
+#endif
