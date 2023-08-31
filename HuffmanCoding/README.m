9120cfa387f0a50629289126bd7135d97db56ede
Hamza Beder <134306865+HamzaBeder1@users.noreply.github.com>
2023-08-31 15:39:37 -0700

    Update README.m

--- HuffmanCoding/README.m
+++ HuffmanCoding/README.m
@@ -1,4 +1,5 @@
-Huffman Coding
+#Huffman Coding
+------
 The purpose of this project is to take a file and compress and decompress it. This is accomplished by building a Huffman Tree using the characters
 in the file, and then traversing this tree to obtain the codes associated with characters. These codes can then be used to compress the file, and then 
 later used to decompress and get the original file.
