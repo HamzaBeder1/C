53144678fc77a055e3e37b94a19c58b91acf25c9
Hamza Beder <134306865+HamzaBeder1@users.noreply.github.com>
2023-08-31 15:35:19 -0700

    added comments

--- HuffmanCoding/Heap.h
+++ HuffmanCoding/Heap.h
@@ -1,6 +1,8 @@
-//
-// Created by Hamza Beder on 8/8/2023.
-//
+/**
+ * @file Heap.h
+ * 
+ * @author Hamza Beder
+*/
 
 #ifndef HEAP_H
 #define HEAP_H
@@ -9,24 +11,40 @@
 #include <vector>
 #include <map>
 #include <queue>
+#include <vector>
 
 class HeapNode {
 public:
+    /**
+     * Instances of HeapNode store a character and its frequency in the file the user asks to compress.
+     * 
+    */
+    HeapNode();
+    HeapNode(char c, int freq);
+
+    //Data each node needs to store
     char c;
     int freq;
     HeapNode * left;
     HeapNode * right;
-    HeapNode();
-    HeapNode(char c, int freq);
 };
 
 class Heap{
 public:
+
+    /**
+     * This class creates a Heap made up of HeapNodes.
+    */
+    Heap(int length);
+
+    //Data associated with a Heap
     int length;
     int heap_size;
     HeapNode ** nodes;
-    Heap(int length);
-    Heap();
+
+    /**
+     * Used to maintain the min-heap property.
+    */
     void minHeapify(int idx);
 };
 
