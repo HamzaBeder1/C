57660015a05db4c02e8efb56e4c64178543247f7
Hamza Beder <134306865+HamzaBeder1@users.noreply.github.com>
2023-08-31 15:37:14 -0700

    added comments

--- HuffmanCoding/Huffman.cpp
+++ HuffmanCoding/Huffman.cpp
@@ -1,11 +1,21 @@
-//
-// Created by Hamza Beder on 8/8/2023.
-//
+/**
+ * @file HuffmanTree.cpp
+ * 
+ * @brief This class performs file compression and decompression using a Huffman Tree
+ * 
+ * @author Hamza Beder
+*/
 
 #include "HuffmanTree.h"
 #include <fstream>
 using namespace std;
 
+/**
+ * This function will read a file and store the characters found along with how often they appear.
+ * @param chars stores the characters found in the file.
+ * @param freqs stores the frequency of characters.
+ * @param path The path to the file to be decompressed.
+*/
 void HuffmanTree:: countFrequency(vector<char> &chars, vector<int> &freqs, string path){
     vector <vector<int>> result;
     vector <int> freqTemp(256,0);
@@ -27,6 +37,19 @@ void HuffmanTree:: countFrequency(vector<char> &chars, vector<int> &freqs, strin
     }
 }
 
+/**
+ * This method creates the Huffman Tree.
+ * 
+ * A priority queue is created using the characters in the file and their associated frequencies.
+ * Then, until a single node remains in the queue, two nodes are popped at a time, a new node is created
+ * with a frequency of the sum of the two popped node's frequencies, the node's children are set to the two extracted
+ * nodes, and then this new node is inserted into the queue.
+ * 
+ * @param chars This is the array of characters in the file.
+ * @param freqs This stores the frequency of characters.
+ * @param path The path to the file to be compressed and decompressed.
+ * 
+*/
 void HuffmanTree:: createTree(vector<char> chars, vector<int> freqs, string path){
     countFrequency(chars, freqs, path);
     PriorityQueue *pq = new PriorityQueue(chars.size());
@@ -42,6 +65,15 @@ void HuffmanTree:: createTree(vector<char> chars, vector<int> freqs, string path
     this->root = pq->extractMin();
 }
 
+/**
+ * This traverses the Huffman Tree using recursion. 
+ * 
+ * Once a leaf node is reached, the hashmaps HuffmanCodes and decompressingMap are updated
+ * to map characters to codes and vice-versa.
+ * 
+ * @param currNode Current node in the traversal of the tree.
+ * @param code This maintians the code associated with the current branch in the tree.
+*/
 void HuffmanTree::getCodes(HeapNode * currNode, string code) {
     if(currNode->left == nullptr && currNode->right == nullptr)
     {
@@ -55,6 +87,11 @@ void HuffmanTree::getCodes(HeapNode * currNode, string code) {
                 getCodes(currNode->right, code+"1");
         }
 }
+
+/**
+ * This compresses a file using the Huffman Codes obtained by traversing the Huffman Tree.
+ * @param path The path of the file to be compressed.
+*/
 
 void HuffmanTree::compress(string path){
     vector<char> chars;
@@ -71,6 +108,14 @@ void HuffmanTree::compress(string path){
     }
 }
 
+/**
+ * This method decompresses the file.
+ * 
+ * It traverses the tree until a root node is reached, storing the code associated with the 
+ * branch it is traversing using DFS.
+ * 
+ * The HashMap decompressingMap is used to obtain the character with the obtained code.
+*/
 void HuffmanTree::decompress(){
     ifstream fin;
     ofstream fout;
@@ -80,7 +125,6 @@ void HuffmanTree::decompress(){
     string code = "";
     HeapNode * temp = this->root;
     while(fin.get(c)){
-
             if(c == '0'){
                 code+="0";
                 temp = temp->left;
