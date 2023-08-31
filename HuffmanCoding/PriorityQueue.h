cee2083a99478ce4d68cf604ce47ccfd0b9eff02
Hamza Beder <134306865+HamzaBeder1@users.noreply.github.com>
2023-08-31 15:36:07 -0700

    added comments

--- HuffmanCoding/PriorityQueue.h
+++ HuffmanCoding/PriorityQueue.h
@@ -1,6 +1,8 @@
-//
-// Created by Hamza Beder on 8/8/2023.
-//
+/**
+ * @file PriorityQueue.h
+ * 
+ * @author Hamza Beder
+*/
 
 #ifndef PRIORITYQUEUE_H
 #define PRIORITYQUEUE_H
@@ -9,13 +11,31 @@
 
 class PriorityQueue {
 public:
-    Heap * pq;
+    
     PriorityQueue(int size);
+
+    Heap * pq;
+
+    /**
+     * This method will inserts newNode into the priority queue while maintaining the min-heap property.
+    */
     void insert(HeapNode * newNode);
+
+    /**
+     * This method will create the priority queue using the characters in the file and their frequencies.
+    */
     void createQueue(std::vector <char> chars, std::vector<int> freqs);
+
+    /**
+     * This method will get the minimum value in the priority queue and ensure 
+     * that the min-heap property is maintained afterwards.
+    */
     HeapNode * extractMin();
-    HeapNode * heapMin();
+
+    /**
+     * This method will return the minimum valued node in the priorty queue without removing it.
+    */
+    HeapNode * queueMin();
 };
 
-
-#endif //HUFFMANCODING_PRIORITYQUEUE_H
+#endif
